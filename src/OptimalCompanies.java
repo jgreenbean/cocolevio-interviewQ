0e88cd7afe035586ba82c2c30f56488c307f27b2
LAPTOP-6RB3F751\Jasmine <jasmine.green@utexas.edu>
2017-08-09 12:01:13 -0500

    built knapsack-like table

--- src/OptimalCompanies.java
+++ src/OptimalCompanies.java
@@ -3,22 +3,26 @@ import java.util.ArrayList;
 public class OptimalCompanies {
 	
 	public static ArrayList<Object> materialRequests;
+	public static int[][] table;
 	
 	static class companyNode {
-		private String name;
-		private int amount;
-		private int price;
+		String name;
+		int amount;
+		int price;
 		
 		public companyNode(String n, int a, int p) {
 			name = n;
 			amount = a;
 			price = p;
 		}
+		
 	}
 	
 	public static void main(String[] args) {
+		int totalMaterial = 20; // total material company can provide
 		materialRequests = new ArrayList();
 		addCompanies();
+		table = buildKnapsackTable(totalMaterial);
 	}
 
 	private static void addCompanies() {
@@ -33,5 +37,24 @@ public class OptimalCompanies {
 		materialRequests.add(new companyNode("I", 9, 24));
 		materialRequests.add(new companyNode("J", 10, 30));
 	}
+	
+	// problem is similar to knapsack problem
+	private static int[][] buildKnapsackTable(int totalMaterial) {
+		int row = materialRequests.size();
+		int col = totalMaterial;
+		int[][] result = new int[row + 1][col + 1];
 
+		for(int r = 1; r <= row; r++) {
+			companyNode currComp = (companyNode) materialRequests.get(r - 1);
+			for(int c = 1; c <= col; c++) {
+				if(currComp.amount > c) {
+					result[r][c] = result[r - 1][c];
+				} else {
+					result[r][c] = Integer.max(result[r - 1][c],
+							(currComp.price + result[r - 1][c - currComp.amount]));
+				}
+			}
+		}
+		return result;
+	}
 }
